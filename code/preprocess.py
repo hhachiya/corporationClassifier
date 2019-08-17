a3a60f88e7a519dc2a8446541508716ae29fdbc8
yaoshunya <newton800pola@gmail.com>
2019-08-17 10:42:45 +0900

    add comment

--- code/preprocess.py
+++ code/preprocess.py
@@ -12,65 +12,61 @@ if __name__ ==  '__main__':
 
     df = pd.read_csv("../data/corporation_sample.csv") #load csv file
 
-    mt = MeCab.Tagger('')
-    mt.parse('') 
-  	 
-    model = word2vec.Word2Vec.load("../data/model/wiki.model")
+    mt = MeCab.Tagger('') #mecabでのパラメーター選択
+    mt.parse('')
 
-    title = df['title']
-    description = df['description']
+    model = word2vec.Word2Vec.load("../data/model/wiki.model") #モデルの読み込み
+
+    title = df['title'] #タイトルの全データ
+    description = df['description'] #説明の全データ
     y = df['class'] #0 or 1
     data_len = title.shape[0]
 
-    out_title = []
-    out_description = [] 
-    
-    
-    for i in range(data_len):
-        title_mecab = mt.parse(title[i])
-        description_mecab = mt.parse(description[i])
-        parts_title = []
-        parts_description = []
+    out_title = [] #タイトルの全データの特徴量が入る
+    out_description = [] #内容の全データの特徴量が入る
 
-        ##########Morphological analysis about title###########
-        ##########and word2vec#################################
-        for row in title_mecab.split("\n"):
-            word = row.split("\t")[0]
-            if word == "EOS":  
+
+    for i in range(data_len): #データ数分forループ
+        title_mecab = mt.parse(title[i]) #titl[i]を分かち書き
+        description_mecab = mt.parse(description[i]) #description[i]を分かち書き
+        parts_title = [] #タイトルごとの特徴量が入る
+        parts_description = [] #内容ごとの特徴量が入る
+
+        #タイトルに関して形態素解析を行い、word2vecを用いて特徴量を抽出
+        for word_mecab in title_mecab.split("\n"): #改行ごとに単語を取得・・・（１）
+            word = word_mecab.split("\t")[0] #タブごとに文章を分割
+            if word == "EOS": #最後の単語になれば、（１）を終える
                 break
             else:
                 pos = row.split("\t")[1]
-                slice = pos.split(",")
-                if slice[0] in ["名詞","動詞","形容詞"]: #except special charactor
+                slice = pos.split(",") #単語の品詞を取得
+                if slice[0] in ["名詞","動詞","形容詞"]: #特定の品詞のみをword2vecにかける
                     try:
-                        word_vec = model.__dict__['wv'][word] #word to vec
-                        parts_title.append(word_vec)
-                    except:
+                        word_vec = model.__dict__['wv'][word] #単語をベクトルに変換　(200,)
+                        parts_title.append(word_vec) #単語ごとに配列に追加
+                    except: #辞書に存在しない言葉があった場合、ベクトルに変換できない
                         pass
 
-        ##########Morphological analysis about description#####
-        ##########and word2vec#################################
-        for row in description_mecab.split("\n"):
-            word = row.split("\t")[0]
-            if word == "EOS":
+        #タイトルに関して形態素解析を行い、word2vecを用いて特徴量を抽出
+        for word_mecab in description_mecab.split("\n"): #改行ごとに単語を取得・・・（１）
+            word = word_mecab.split("\t")[0] #タブごとに文章を分割
+            if word == "EOS": #最後の単語になれば、（１）を終える
                 break
             else:
                 pos = row.split("\t")[1]
-                slice = pos.split(",")
-                if slice[0] in ["名詞","動詞","形容詞"]: #except special charactor
+                slice = pos.split(",") #単語の品詞を取得
+                if slice[0] in ["名詞","動詞","形容詞"]: #特定の品詞のみをword2vecにかける
                     try:
-                        word_vec = model.__dict__['wv'][word] #word to vec
-                        parts_description.append(word_vec)
-                    except:
+                        word_vec = model.__dict__['wv'][word] #単語をベクトルに変換　(200,)
+                        parts_description.append(word_vec) #単語ごとに配列に追加
+                    except: #辞書に存在しない言葉があった場合、ベクトルに変換できない
                         pass
- 
-        out_title.append(parts_title)
-        out_description.append(parts_description)
+
+        out_title.append(parts_title) #タイトルごとに配列に追加
+        out_description.append(parts_description) #説明ごとに配列に追加
         #pdb.set_trace()
-
+        #pickleでout_title,out_out_descriptionを保存
         with open('../data/out/title_vec.pickle','wb') as f:
             pickle.dump(out_title,f)
         with open('../data/out/description_vec.pickle','wb') as f:
             pickle.dump(out_description,f)
-        
-    
