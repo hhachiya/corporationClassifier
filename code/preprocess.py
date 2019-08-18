96d482f418a1bc8baf02ffdf936e4fa5e5564a6e
yaoshunya <newton800pola@gmail.com>
2019-08-18 22:37:12 +0900

    add comment at prepocess.py

--- code/preprocess.py
+++ code/preprocess.py
@@ -7,32 +7,40 @@ from gensim.models import word2vec
 import pickle
 
 def make_x_by_ave(title,description):
-    data_len = len(title)
-    for i in range(data_len):
-        title_len = len(title[i])
-        description_len = len(description[i])
+    data_len = len(title) #データの数
+    #-------------------------
+    #title,descriptionごとの処理
+    for i in range(data_len): #データ数だけfor文
+        title_len = len(title[i]) #title[i]の長さ
+        description_len = len(description[i]) #description[i]の長さ
 
-        feature_title_parts = np.zeros_like(title[0][0])
-        feature_description_parts = np.zeros_like(description[0][0])
-        
-        for t in range(title_len):
-            feature_title_parts = np.add(feature_title_parts,title[i][t])
-        if title_len > 0:
-            feature_title_parts = np.divide(feature_title_parts,title_len)
+        feature_title_parts = np.zeros_like(title[0][0]) #title[i]の中の特徴量をすべて足し合わせ、最後にtitle[i]の長さで割る(平均)...(1)
+        feature_description_parts = np.zeros_like(description[0][0]) #description[i]の中の特徴量をすべて足し合わせ、最後にdescription[i]の長さで割る(平均)...(2)
 
-        for d in range(description_len):
-            feature_description_parts = np.add(feature_description_parts,description[i][d])
-        if description_len > 0:
-            feature_description_parts = np.divide(feature_description_parts,description_len)
-        
+        #-------------------------
+        #title[i]ごとにデータを処理
+        for t in range(title_len): #title[i]の長さ分だけfor文
+            feature_title_parts = np.add(feature_title_parts,title[i][t]) #(1)
+        if title_len > 0: #0では割れないを避ける
+            feature_title_parts = np.divide(feature_title_parts,title_len) #(1)
+        #description[i]ごとにデータを処理
+        for d in range(description_len): #description[i]の長さ分だけfor文
+            feature_description_parts = np.add(feature_description_parts,description[i][d]) #(2)
+        if description_len > 0: #0では割れないを避ける
+            feature_description_parts = np.divide(feature_description_parts,description_len) #(2)
+        #-------------------------
+
+        #-------------------------
+        #feature_title_parts,feature_description_partsをまとめる
         if i == 0:
             feature_title = feature_title_parts[np.newaxis]
             feature_description = feature_description_parts[np.newaxis]
         else:
             feature_title = np.append(feature_title,feature_title_parts[np.newaxis],axis=0)
             feature_description = np.append(feature_description,feature_description_parts[np.newaxis],axis=0)
-
-    return np.append(feature_title[np.newaxis],feature_description[np.newaxis],axis=0)
+        #-------------------------
+    #-------------------------
+    return np.append(feature_title[np.newaxis],feature_description[np.newaxis],axis=0 #feature_title,feature_descriptionをまとめて出力((2(title,description),221(データ数),200(特徴ベクトル)))
 
 
 
@@ -98,14 +106,21 @@ if __name__ ==  '__main__':
         #-------------------------
         out_title.append(parts_title) #タイトルごとに配列に追加
         out_description.append(parts_description) #説明ごとに配列に追加
-        
-    X = make_x_by_ave(out_title,out_description)
-    Y = np.array(y)
-    
+    #-------------------------
+
+    #-------------------------
+    X = make_x_by_ave(out_title,out_description) ##feature_title,feature_descriptionをまとめて出力((2(title,description),221(データ数),200(特徴ベクトル)))
+    Y = np.array(y) #labelをnumpyに変換
+    #-------------------------
+
+    #-------------------------
+    #------データをpickleで保存
     with open('../data/out/data_x.pickle','wb') as f:
         pickle.dump(X,f)
     with open('../data/out/data_y.pickle','wb') as f:
         pickle.dump(Y,f)
+    #-------------------------
+
     """
     #pickleでout_title,out_out_descriptionを保存
     with open('../data/out/title_vec.pickle','wb') as f:
